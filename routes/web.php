5b1104812ef02be0b40fbc8ad8b6421b6154b6a2
Amod D Kulkarni <amod.d.kulkarni@gmail.com>
2023-11-06 11:46:51 +0530

    adding endpoint wh/transactions/status

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SubmerchantController;
+use App\Livewire\CreateSubmerchant;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,6 +19,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/wh/transactions/status', function () {
+    return "success";
+});
+
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
@@ -26,3 +32,14 @@ Route::middleware([
         return view('dashboard');
     })->name('dashboard');
 });
+
+Route::get('/sdk/registration', CreateSubmerchant::class);
+Route::get('/sdk/thankyou', function () { return 'Successfully created merchant with status as INACTIVE . Pending Verification !';});
+
+Route::resource('submerchants', SubmerchantController::class);
+
+//Route::controller(SubmerchantController::class)->group(function () {
+//    Route::get('/submerchants/{id}', 'show');
+//    Route::post('/submerchants', 'store');
+//});
+
