54ff3aaea7ea81f2dbb1a9a8f0fcf8ebfdb63f82
Amod D Kulkarni <amod.d.kulkarni@gmail.com>
2023-11-29 15:21:52 +0530

    dd in else condition of response decrypt

--- app/Actions/GenerateJWS.php
+++ app/Actions/GenerateJWS.php
@@ -102,7 +102,7 @@ class GenerateJWS
     } else {
       //$header = json_decode(base64_decode($jwt_values[0]), true);
       //$payload = json_decode(base64_decode($jwt_values[1]), true);
-      //dd($jwt,$jwt_values,$recieved_signature,$recieved_header_and_payload,$valid_signature,$base64UrlValidSignature,$header,$payload);
+      dd($jwt,$jwt_values,$recieved_signature,$recieved_header_and_payload,$valid_signature,$base64UrlValidSignature,$header,$payload);
       //return json_decode(base64_decode($jwt_values[1]), true);
       return "ER48027";
     }
