d0d88865cb228f030b01706206c48c45949cd0bd
Amod D Kulkarni <amod.d.kulkarni@gmail.com>
2023-11-29 17:23:52 +0530

    getting back the special characs for base64url encoding

--- app/Actions/GenerateJWS.php
+++ app/Actions/GenerateJWS.php
@@ -70,13 +70,10 @@ class GenerateJWS
     Log::info($header);
     Log::info($payload);
 
-    //$base64UrlHeader = str_replace(['+', '/', '='], ['-', '_', ''], base64_encode($header));
-    //$base64UrlPayload = str_replace(['+', '/', '='], ['-', '_', ''], base64_encode($payload));
-    $base64UrlHeader = base64_encode($header);
-    $base64UrlPayload = base64_encode($payload);
+    $base64UrlHeader = str_replace(['+', '/', '='], ['-', '_', ''], base64_encode($header));
+    $base64UrlPayload = str_replace(['+', '/', '='], ['-', '_', ''], base64_encode($payload));
 
-    $signature = hash_hmac('sha256', $base64UrlHeader . "." . $base64UrlPayload, $secret, true);
-    //$base64UrlSignature = str_replace(['+', '/', '='], ['-', '_', ''], base64_encode($signature));
+    $base64UrlSignature = str_replace(['+', '/', '='], ['-', '_', ''], base64_encode($signature));
     $base64UrlSignature = base64_encode($signature);
 
     $jwt = $base64UrlHeader . "." . $base64UrlPayload . "." . $base64UrlSignature;
