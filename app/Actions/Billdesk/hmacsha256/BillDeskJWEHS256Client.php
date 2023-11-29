dfe6796134a28f2eaeb2dd052e36f8018f633ca2
Amod D Kulkarni <amod.d.kulkarni@gmail.com>
2023-11-29 17:15:01 +0530

    dd

--- app/Actions/Billdesk/hmacsha256/BillDeskJWEHS256Client.php
+++ app/Actions/Billdesk/hmacsha256/BillDeskJWEHS256Client.php
@@ -88,20 +88,16 @@ try {
   Log::info("===============CLIENT EXCEPTION ===== STARTS ======================");
   Log::info($e);
   Log::info("~~~~~~~~~~~~~~~CLIENT EXCEPTION ~~~~~~ENDS ~~~~~~~~~~~~~~~");
-
-//  dd(GenerateJWS::decryptPG($e->getResponse()->getBody()->getContents()),$this->jweHelper->verifyAndDecrypt($e->getResponse()->getBody()->getContents()));
-//  Log::info($e->getRequest());
-  //  Log::info($e->getResponse());
-  //
+  dd(GenerateJWS::decryptPG($token),$this->jweHelper->verifyAndDecrypt($token));
   return GenerateJWS::decryptPG($e->getResponse()->getBody()->getContents());
 } catch (ServerException $e) {
   Log::info("===============SERVER EXCEPTION ===== STARTS ======================");
   Log::info($e);
   Log::info("~~~~~~~~~~~~~~~SERVER EXCEPTION ~~~~~~ENDS ~~~~~~~~~~~~~~~");
-//  Log::info($e->getRequest());
-  //  Log::info($e->getResponse());
-  //
-  //dd(GenerateJWS::decryptPG($e->getResponse()->getBody()->getContents()),$this->jweHelper->verifyAndDecrypt($e->getResponse()->getBody()->getContents()));
+
+  $token = $e->getResponse()->getBody()->getContents();
+
+  dd(GenerateJWS::decryptPG($token),$this->jweHelper->verifyAndDecrypt($token));
   return GenerateJWS::decryptPG($e->getResponse()->getBody()->getContents());
 }
 
