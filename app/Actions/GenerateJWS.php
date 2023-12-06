d1e379f149ba0f3facbf8f36c7bece761d66d951
Amod D Kulkarni <amod.d.kulkarni@gmail.com>
2023-12-06 15:40:18 +0530

    new welcome content

--- app/Actions/GenerateJWS.php
+++ app/Actions/GenerateJWS.php
@@ -73,8 +73,8 @@ class GenerateJWS
     $base64UrlHeader = str_replace(['+', '/', '='], ['-', '_', ''], base64_encode($header));
     $base64UrlPayload = str_replace(['+', '/', '='], ['-', '_', ''], base64_encode($payload));
 
+    $signature = hash_hmac('sha256', $base64UrlHeader . "." . $base64UrlPayload, $secret, true);
     $base64UrlSignature = str_replace(['+', '/', '='], ['-', '_', ''], base64_encode($signature));
-    $base64UrlSignature = base64_encode($signature);
 
     $jwt = $base64UrlHeader . "." . $base64UrlPayload . "." . $base64UrlSignature;
 
